55d7fc3d897c6e6400df93e608997b47f86765c4
ivan0w <95546800+ivan0w@users.noreply.github.com>
2022-03-15 14:11:45 +0100

    Update client.lua

--- ragdoll/client.lua
+++ ragdoll/client.lua
@@ -2,7 +2,7 @@ Citizen.CreateThread(function()
  	while true do
  		Citizen.Wait(0)
  		if IsControlPressed(1, 311) then
- 		SetPedToRagdoll(GetPlayerPed(-1), 1000, 1000, 0, 0, 0, 0)
+ 		SetPedToRagdoll(GetPlayerPed(-1), 1000, 1000, 0, true, true, false)
  		end
  	end
  end)
